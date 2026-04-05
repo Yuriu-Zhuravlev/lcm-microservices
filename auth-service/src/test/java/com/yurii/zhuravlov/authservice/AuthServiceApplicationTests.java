04c721e2c15dbc5d415357b32bc646794b7c7a5c
Журавльов Юрій <nagatoura@gmail.com>
2026-04-05 17:10:14 +0300

    initial commit

---
+++ auth-service/src/test/java/com/yurii/zhuravlov/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yurii.zhuravlov.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
