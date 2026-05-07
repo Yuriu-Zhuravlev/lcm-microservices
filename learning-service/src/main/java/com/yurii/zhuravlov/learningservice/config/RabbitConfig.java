08ecb132e487c80815fdde6d800333d88c1bc4f7
Журавльов Юрій <nagatoura@gmail.com>
2026-05-07 10:54:07 +0300

    added durable queues

--- learning-service/src/main/java/com/yurii/zhuravlov/learningservice/config/RabbitConfig.java
+++ learning-service/src/main/java/com/yurii/zhuravlov/learningservice/config/RabbitConfig.java
@@ -1,11 +1,8 @@
 package com.yurii.zhuravlov.learningservice.config;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.FanoutExchange;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,9 +10,15 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitConfig {
     public static final String COURSE_QUEUE = "learning.course.updates";
     public static final String COURSE_EXCHANGE = "course.exchange";
+    public static final String COURSE_DLQ = "learning.course.updates.dlq";
+    public static final String COURSE_DLX = "learning.course.updates.dlx";
 
     @Bean
-    public Queue courseQueue() { return new Queue(COURSE_QUEUE); }
+    public Queue courseQueue() {
+        return QueueBuilder.durable(COURSE_QUEUE)
+                .withArgument("x-dead-letter-exchange", COURSE_DLX)
+                .build();
+    }
 
     @Bean
     public FanoutExchange courseExchange() { return new FanoutExchange(COURSE_EXCHANGE); }
@@ -23,6 +26,21 @@ public class RabbitConfig {
     @Bean
     public Binding binding(Queue courseQueue, FanoutExchange courseExchange) {
         return BindingBuilder.bind(courseQueue).to(courseExchange);
+    }
+
+    @Bean
+    public Queue deadLetterQueue() {
+        return QueueBuilder.durable(COURSE_DLQ).build();
+    }
+
+    @Bean
+    public FanoutExchange deadLetterExchange() {
+        return new FanoutExchange(COURSE_DLX);
+    }
+
+    @Bean
+    public Binding dlqBinding() {
+        return BindingBuilder.bind(deadLetterQueue()).to(deadLetterExchange());
     }
 
     @Bean
