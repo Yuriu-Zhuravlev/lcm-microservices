514bc567c9fff77b5ce2b7a4879ac4346def1bf6
Журавльов Юрій <nagatoura@gmail.com>
2026-06-13 21:33:25 +0300

    tests fix

--- learning-service/src/test/java/com/yurii/zhuravlov/learningservice/integration/EnrollmentServiceCacheTest.java
+++ learning-service/src/test/java/com/yurii/zhuravlov/learningservice/integration/EnrollmentServiceCacheTest.java
@@ -22,6 +22,7 @@ import java.util.Map;
 import static java.util.Map.entry;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -82,7 +83,7 @@ class EnrollmentServiceCacheTest extends BaseIntegrationTest {
 
         enrollmentService.enrollUser(userId, 20L);
 
-        when(courseServiceClient.getAllCoursesByIds(List.of(10L, 20L)))
+        when(courseServiceClient.getAllCoursesByIds(anyList()))
                 .thenReturn(List.of(mockCourse(10L), mockCourse(20L)));
         ListEnrollmentResponses result = enrollmentService.getEnrollmentsByUserId(userId);
         assertThat(result.enrollmentResponses()).hasSize(2);
